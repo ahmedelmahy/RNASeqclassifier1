02ed0b554c3c5a82651d058a28eef0bb58a3175e
ahmedelmahy <elmahy2005@gmail.com>
2018-03-16 13:52:15 +0100

    test

--- R/filters.R
+++ R/filters.R
@@ -1,8 +1,9 @@
 filter0 <- function(counts, class){
-  r <- rowMeans(counts)
-  r <- sort(r,decreasing = TRUE)
-  r <- r[1:10000]
-
-  # subset data
-  keep <- which(rownames(counts) %in% r)
+  print("hi")
+  # r <- rowMeans(counts)
+  # r <- sort(r,decreasing = TRUE)
+  # r <- r[1:10000]
+  #
+  # # subset data
+  # keep <- which(rownames(counts) %in% r)
 }
