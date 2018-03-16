dbcfc5534fd1ad9c3f93a78836495829bfb4e61a
ahmedelmahy <elmahy2005@gmail.com>
2018-03-16 14:34:11 +0100

    filter0

--- R/filters.R
+++ R/filters.R
@@ -1,9 +1,8 @@
-filter0 <- function(counts, class){
-  print("hi")
-  # r <- rowMeans(counts)
-  # r <- sort(r,decreasing = TRUE)
-  # r <- r[1:10000]
-  #
-  # # subset data
-  # keep <- which(rownames(counts) %in% r)
+filter0 <- function(counts, keepn = 10000){
+  r <- rowMeans(counts)
+  r <- sort(r,decreasing = TRUE)
+  r <- r[1:keepn]
+  # subset data
+  keep <- which(rownames(counts) %in% r)
+
 }
