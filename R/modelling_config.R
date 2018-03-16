e111461a43e42af6cb19e325879b6435a7bf2a27
ahmedelmahy <elmahy2005@gmail.com>
2018-03-16 16:10:09 +0100

    k

---
+++ R/modelling_config.R
@@ -0,0 +1,21 @@
+split_counts <- function(counts, class){
+  nr <- 1:ncol(counts)
+  nr_t <- as.integer(ncol(counts) * .8)
+  s <- sample(nr, nr_t)
+  train <- counts[,s]
+  train_class <- class[s]
+  test <- counts[,-s]
+  test_class <- class[-s]
+  rm(nr, nr_t,s)
+  return(list(train = train, train_class = train_class,
+              test = test, test_class = test_class))
+}
+
+config_caret <- function(...){
+  doMC::registerDoMC(cores = 15)
+  fitControl <- caret::trainControl(method = "cv",
+                                    classProbs = T,
+                                    savePredictions = T,
+                                    verboseIter = TRUE, allowParallel = TRUE,...)
+  return(fitControl)
+}
