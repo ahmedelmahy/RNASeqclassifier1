e764e2829e6108fe405f6cb8804a603b15da60d3
ahmedelmahy <elmahy2005@gmail.com>
2018-03-17 15:31:49 +0100

    .

--- R/dge.R
+++ R/dge.R
@@ -7,8 +7,16 @@ normalize_voom <- function(counts){
   dge <- calcNormFactors(dge)
   v <- voom(dge)
   return(v)
+  return(v)
 }
 
+#' Select diffentially expressed genes(DGEs) from a voom object
+#'
+#' @param v A voom object
+#' @param class A vector of assigned class for each sample e.g. "disease" or
+#' "control"
+#' @param keepn Number of DEGs to return
+#' @return A vector of selected DEGs
 deg_voom <- function(v,class, keepn = 10){
   design <- model.matrix(~class)
   fit <- lmFit(v, design)
