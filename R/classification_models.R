f1bf62496b41bf2586e49f3cda471c12a8c43e05
ahmedelmahy <elmahy2005@gmail.com>
2018-03-19 00:29:42 +0100

    .

---
+++ R/classification_models.R
@@ -0,0 +1,67 @@
+fit_svmRadial <- function(x, y, fitConrol){
+  fit_tune <- train(x = x,
+                    y = y,
+                    method = "svmRadial",
+                    trControl = fitControl)
+
+  fit <- train(x = x,
+               y = y,
+               method = "svmRadial",
+               trControl = fitControl,
+               tuneGrid=expand.grid(.sigma = fit_tune$bestTune$sigma,
+                                    .C=fit_tune$bestTune$C),
+               metric = "ROC")
+  return(fit)
+}
+
+
+fit_svmLinear <- function(x, y, fitConrol){
+  fit_tune <- train(x = x,
+                    y = y,
+                    method = "svmLinear",
+                    trControl = fitControl)
+
+  fit <- train(x = x,
+               y = y,
+               method = "svmLinear",
+               trControl = fitControl,
+               tuneGrid=expand.grid(.sigma = fit_tune$bestTune$sigma,
+                                    .C=fit_tune$bestTune$C),
+               metric = "ROC")
+  return(fit)
+}
+
+
+fit_glm <- function(x,y, fitControl){
+  fit_tune <- train(x = x,
+                    y = y,
+                    method = "glmnet",
+                    trControl = fitControl)
+
+  fit <- caret::train(x = x,
+                      y = y,
+                      method ="glmnet",
+                      tuneGrid=expand.grid(.alpha = fit_tune$bestTune$alpha,
+                                           .lambda = fit_tune$bestTune$lambda),
+                      trControl = fitControl)
+  return(fit)
+}
+
+fit_rf <- function(x,y, fitControl){
+  fit_tune <- train(x = x,
+                    y = y,
+                    method = "rf",
+                    trControl = fitControl)
+
+  fit <- caret::train(x = x,
+                      y = y,
+                      method ="rf",
+                      tuneGrid=expand.grid(.alpha = fit_tune$bestTune$alpha,
+                                           .lambda = fit_tune$bestTune$lambda),
+                      trControl = fitControl)
+  return(fit)
+}
+
+
+
+
--- R/plots.R
+++ R/plots.R
@@ -31,3 +31,82 @@ pca <- function(counts, class, multiple = FALSE, ...){
 
   gridExtra::grid.arrange(p1,p2,p3,p4,p5,p6)
 }}
+
+
+
+compare_selected_features_barplot <- function(selected_genes_df, with){
+  switch (with,
+          yes = {
+            if (class(selected_genes_df[[1]]) == "numeric"){
+              x <- scale(c(selected_genes_df[[1]],selected_genes_df[[3]]))
+              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
+              df <- data.frame(x, class = class)
+              colnames(df) <- c(names(selected_genes_df[[1]])[1], "class")
+              df <- df[order(df$class),]
+              df$samplenum <- c(1: dim(df)[1])
+              df <-melt(df,id = c("samplenum","class"))
+
+              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
+                geom_col() +
+                facet_grid(variable~.)
+            }else{
+              x <- scale(rbind(selected_genes_df[[1]],selected_genes_df[[3]]))
+              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
+              df <- data.frame(x, class = class)
+              df <- df[order(df$class),]
+              df$samplenum <- c(1: dim(df)[1])
+              df <-melt(df,id = c("samplenum","class"))
+
+              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
+                geom_col() +
+                facet_grid(variable~.)
+
+            }},
+          no = {
+            if (class(selected_genes_df[[1]]) == "numeric"){
+              x <- c(selected_genes_df[[1]],selected_genes_df[[3]])
+              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
+              df <- data.frame(x, class = class)
+              colnames(df) <- c(names(selected_genes_df[[1]])[1], "class")
+              df <- df[order(df$class),]
+              df$samplenum <- c(1: dim(df)[1])
+              df <-melt(df,id = c("samplenum","class"))
+
+              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
+                geom_col() +
+                facet_grid(variable~.)
+            }else{
+              x <- rbind(selected_genes_df[[1]],selected_genes_df[[3]])
+              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
+              df <- as.data.frame(cbind(x, class))
+              df <- df[order(df$class),]
+              df$samplenum <- c(1: dim(df)[1])
+              df <-melt(df,id = c("samplenum","class"))
+
+              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
+                geom_col() +
+                facet_grid(variable~.)
+            }}
+  )
+  print(p1)
+}
+
+
+plot_multipe_rocs <- function(model_title_list){
+  results_list_roc <- list(NA)
+  for(i in 1:length(model_title_list)){
+    myRoc_on_train_data <- roc(predictor = model_title_list[[i]]$pred$lym,
+                               response = model_title_list[[i]]$pred$obs)
+    message(auc(myRoc_on_train_data))
+    results_list_roc[[i]] <- data_frame(
+      True_positive_rate = myRoc_on_train_data$sensitivities,
+      False_positive_rate = 1 - myRoc_on_train_data$specificities,
+      modell = model_title_list[[i]]$modelInfo$label)
+  }
+  results_df_roc <- bind_rows(results_list_roc)
+
+  ggplot(aes(x = False_positive_rate,  y = True_positive_rate, group = modell), data = results_df_roc) +
+    geom_line(aes(color = modell), size = 1) +
+    geom_abline(intercept = 0, slope = 1, color = "gray", size = 1) +
+    theme_bw(base_size = 18)
+}
