652c33e13d327b7b737b2c83d2fca71b83c1cd27
ahmedelmahy <elmahy2005@gmail.com>
2018-03-19 13:51:11 +0100

    .

--- R/plots.R
+++ R/plots.R
@@ -34,59 +34,18 @@ pca <- function(counts, class, multiple = FALSE, ...){
 
 
 
-compare_selected_features_barplot <- function(selected_genes_df, with){
-  switch (with,
-          yes = {
-            if (class(selected_genes_df[[1]]) == "numeric"){
-              x <- scale(c(selected_genes_df[[1]],selected_genes_df[[3]]))
-              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
-              df <- data.frame(x, class = class)
-              colnames(df) <- c(names(selected_genes_df[[1]])[1], "class")
-              df <- df[order(df$class),]
-              df$samplenum <- c(1: dim(df)[1])
-              df <-melt(df,id = c("samplenum","class"))
+compare_selected_features_barplot <- function(df, class){
 
-              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
-                geom_col() +
-                facet_grid(variable~.)
-            }else{
-              x <- scale(rbind(selected_genes_df[[1]],selected_genes_df[[3]]))
-              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
-              df <- data.frame(x, class = class)
-              df <- df[order(df$class),]
-              df$samplenum <- c(1: dim(df)[1])
-              df <-melt(df,id = c("samplenum","class"))
+  df <- as.data.frame(df)
+  df$class <- class
+  df <- df[order(df$class),]
+  df$samplenum <- c(1: dim(df)[1])
+  df <-melt(df,id = c("samplenum","class"))
 
-              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
-                geom_col() +
-                facet_grid(variable~.)
+  p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
+    geom_col() +
+    facet_grid(variable~.)
 
-            }},
-          no = {
-            if (class(selected_genes_df[[1]]) == "numeric"){
-              x <- c(selected_genes_df[[1]],selected_genes_df[[3]])
-              class <- as.factor(c(as.character(selected_genes_df[[2]]),as.character(selected_genes_df[[4]])))
-              df <- data.frame(x, class = class)
-              colnames(df) <- c(names(selected_genes_df[[1]])[1], "class")
-              df <- df[order(df$class),]
-              df$samplenum <- c(1: dim(df)[1])
-              df <-melt(df,id = c("samplenum","class"))
-
-              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
-                geom_col() +
-                facet_grid(variable~.)
-            }else{
-              # the one that works
-              df <- as.data.frame(cbind(x, class))
-              df <- df[order(df$class),]
-              df$samplenum <- c(1: dim(df)[1])
-              df <-melt(df,id = c("samplenum","class"))
-
-              p1 <- ggplot(df, aes(samplenum, value, fill=class)) +
-                geom_col() +
-                facet_grid(variable~.)
-            }}
-  )
   print(p1)
 }
 
