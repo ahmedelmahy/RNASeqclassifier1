caf2d4c131e8f178f28a3976a59244cba1095a6b
ahmedelmahy <elmahy2005@gmail.com>
2018-03-25 13:56:34 +0200

    .

--- R/feature_selection_by_information.R
+++ R/feature_selection_by_information.R
@@ -5,7 +5,7 @@
 #'@return a vector of selected genes
 filter_step <- function(counts, class, direction = "backward"){
   df <- as.data.frame(t(counts))
-  df$class <- as.factor(class2)
+  df$class <- as.factor(class)
   fullmod = glm(class ~.,data = df,family=binomial)
   backwards = step(fullmod, direction = direction)
   genes <- names(coef(backwards))[-1]
