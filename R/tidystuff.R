74cb00b834545594c8b28026d8227601e39fb9a9
ahmedelmahy <elmahy2005@gmail.com>
2018-04-01 16:55:04 +0200

    .

--- R/tidystuff.R
+++ R/tidystuff.R
@@ -1,7 +1,8 @@
 #' Clean a class vector by keeping only alphabetical letters
 #' to avoid errors when we use the class as a y variable in
 #' ml models
-#' @param class
+#' @param class A class vector
+#' @return vector A tidy version of the class vector
 tidyClass <- function(class){
   return(gsub("[^a-zA-Z]","",class))
 }
